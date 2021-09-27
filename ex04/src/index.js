9702273643e06c386d561bd157ebcc0857aae205
kenoaljiji <studioaljiji@gmail.com>
2021-09-28 00:33:32 +0200

    mutate array with using const variable

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -1,16 +1,16 @@
-function printManyTimes(str) {
+const milili = [10, 25, 4];
+
+function myArr(mili) {
   "use strict";
 
   // only change code below this line
-
-  const SENTENCE = str + " is cool!";
-  for (let i = 0; i < str.length; i += 2) {
-    console.log(SENTENCE);
-  }
-  return SENTENCE;
-
+  mili[0] = 4;
+  mili[1] = 10;
+  mili[2] = 25;
   // only change code above this line
+
+  return mili;
 }
 
-printManyTimes("Arena");
-module.exports = printManyTimes;
+console.log(myArr(milili));
+module.exports = myArr;
